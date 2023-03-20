cea288c7dc3b440572ea3b1e09bfb05c5d3079ad
Eddie <edpoulsonv@hotmail.com>
2023-03-20 10:47:51 -0700

    feat: image carousel, wip on everything else

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,9 @@
     <body class="bg-black">
         <div id="container" class="flex flex-column justify-center">
             <div id="nav" class="flex flex-row border-4 rounded-lg border-white">
-                <a class="font-mono text-white hover:opacity-75 m-2" href="/">home</a>
-                <a class="font-mono text-white hover:opacity-75 m-2" href="/contact">contact me</a>
-                <a class="font-mono text-white hover:opacity-75 m-2" href="/projects">projects</a>
+                <a class="nav-btn" href="/">home</a>
+                <a class="nav-btn" href="/contact">contact me</a>
+                <a class="nav-btn" href="/projects">projects</a>
             </div>
             
         </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
 <script>
-    import { fly } from 'svelte/transition';
+    import { fade, fly } from 'svelte/transition';
     import { onMount } from 'svelte';
+
+    const pics = [
+        "/assets/coolguy.jpg",
+        'https://picsum.photos/300/200?random=1',
+        'https://picsum.photos/300/200?random=2'
+    ]
+
+    let index = 0;
+
+    function next() {
+        index = (index + 1) % pics.length;
+    }
+
+    function back() {
+        index = (index - 1) % pics.length;
+    }
 
     let loaded = false;
     onMount(() => loaded = true);
@@ -12,7 +28,24 @@
 
 <div class="flex flex-col justify-center items-center">
     {#if loaded}
-        <h1 transition:fly="{{ y: 20, duration: 500 }}" class="text-white mt-5 font-bold text-2xl">Hi, I'm Eddie!</h1>
+        <h1 transition:fly={{ y: 20, duration: 500 }} class="text-white mt-5 font-bold text-2xl font-mono">Hi, I'm Eddie!</h1>
     {/if}
-    <img src="/assets/coolguy.jpg" alt="" class="mt-4 h-[40rem] rounded-md">
+    <!-- Gonna have to figure out how to make an image carousel and how to sorta upload images maybe sorta autonomously -->
+    <div class="flex flex-row" transition:fade={{ delay: 150 }}>
+        {#if index > 0}
+            <button class="text-white m-2" on:click={back}>&#60;</button>
+        {:else}
+            <button class="invisible m-2">&#60;</button>
+        {/if}
+
+        {#each [pics[index]] as pic}
+            <img src={pic} alt="" class="mt-4 h-96 w-[230px] md:w-96 md:h-[40rem] rounded-md">
+        {/each}
+        {#if index != pics.length - 1}
+            <button class="text-white m-2" on:click={next}>&#62;</button>
+        {:else}
+            <button class="invisible m-2">&#60;</button>
+        {/if}
+    </div>
+    
 </div>
