6e8090a6a436f708b68fba2f78f75617159e85e6
Eddie <edpoulsonv@hotmail.com>
2023-03-20 14:57:34 -0700

    added a route

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,4 +4,3 @@ import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private';
 const supabaseUrl = SUPABASE_URL;
 const supabaseKey = SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
-console.log(SUPABASE_KEY)
