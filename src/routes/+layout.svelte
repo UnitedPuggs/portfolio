60dde133d01d50e7c5e81ade5b8bdb2e60c0cd35
UnitedPuggs <edpoulsonv@hotmail.com>
2023-04-19 15:29:16 -0700

    feat: dropdown animations, sound when changing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,24 @@
 <script>
   import "../app.css";
+
+  let audio;
+
+  function playClick() {
+    audio.play();
+  }
 </script>
 
 <nav class="mt-2">
     <body class="bg-black">
         <div id="container" class="flex flex-column flex-wrap justify-center">
             <div id="nav" class="flex flex-row border-4 rounded-lg border-white">
-                <a class="nav-btn" href="/">home</a>
-                <a class="nav-btn" href="/contact">contact me</a>
-                <a class="nav-btn" href="/projects">projects</a>
+                <a class="nav-btn" href="/" on:click={playClick}>home</a>
+                <a class="nav-btn" href="/contact" on:click={playClick}>contact me</a>
+                <a class="nav-btn" href="/projects" on:click={playClick}>projects</a>
             </div>
             
         </div>
     </body>
 </nav>
+<audio src="https://cdn.freesound.org/previews/536/536108_10912485-lq.mp3" bind:this={audio}></audio>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { fade, fly } from 'svelte/transition';
+    import { fade, fly, slide } from 'svelte/transition';
     import { onMount } from 'svelte';
+  import { transition_in } from 'svelte/internal';
     
     let pics = []
     let index = 0;
@@ -9,6 +10,10 @@
     let experienceVis = false;
     let aboutMeVis = false;
     let technicalVis = false;
+
+    let expButton = "experience  v";
+    let aboutMeButton = "about me  v";
+    let technicalButton = "technical skills  v";
 
     async function get_image_urls() {
         const res = await fetch("/image");
@@ -29,14 +34,26 @@
 
     function toggleExperience() {
         experienceVis = !experienceVis;
+        if(experienceVis == true)
+            expButton = "experience  ʌ"
+        else 
+            expButton = "experience  v";
     }
 
     function toggleAboutMe() {
         aboutMeVis = !aboutMeVis;
+        if(aboutMeVis == true)
+            aboutMeButton = "about me  ʌ"
+        else 
+            aboutMeButton = "about me  v";
     }
 
     function toggleTechnical() {
         technicalVis = !technicalVis;
+        if(technicalVis == true)
+            technicalButton = "technical skills  ʌ"
+        else 
+            technicalButton = "technical skills  v";
     }
 
     onMount(() => {
@@ -74,19 +91,22 @@
         {/if}
     </div>
     <div class="text-white mt-20 mb-10 flex flex-col text-left max-w-3xl" transition:fade={{ delay: 150 }}>
-        <button class="hover:opacity-75 m-1" on:click={toggleExperience}>experience  v</button>
+        <button class="hover:opacity-75 m-1" on:click={toggleExperience}>{expButton}</button>
         {#if experienceVis}
+            <div transition:slide>
             <p><strong>Clubspeed</strong> - Aug. 2021 - Now<br>
             <em>IT Support Engineer / Automation Specialist</em><br>
                <ul class="list-disc pl-5">
                 <li>Provided technical support for the company's software and troubleshot associated hardware</li>
                 <li>Created complex T-SQL queries and scripts to efficiently automate the manipulation of data for MSSQL servers associated with the company's software.</li>
-                <li>Working on an <a href="https://github.com/UnitedPuggs/SupportSearch">internal tool</a> (with a helper <a href="https://github.com/UnitedPuggs/SupportSearch-Admin">admin site</a>)
+                <li>Working on an <a href="https://github.com/UnitedPuggs/SupportSearch" class="underline hover:no-underline">internal tool</a> (with a helper <a href="https://github.com/UnitedPuggs/SupportSearch-Admin" class="underline hover:no-underline">admin site</a>)
                     in the form of a Chrome extension that utilizes internal APIs to more efficiently server useful customer information.</li>
                </ul>
+            </div>
         {/if}
-        <button class="hover:opacity-75 m-1" on:click={toggleAboutMe}>about me  v</button>
+        <button class="hover:opacity-75 m-1" on:click={toggleAboutMe}>{aboutMeButton}</button>
         {#if aboutMeVis}
+            <div transition:slide>
             <p class="text-gray-40">
                 I'm a computer science student of CSU - Fullerton with an expected graduation date of Dec. 2023! I'm currently looking for full-time roles starting
                 in June of 2023, as I'll be "graduated" by May (I only have one class in fall). I love learning and intend to finish up a degree in Electrical Engineering and another in Economics once I've got 
@@ -94,15 +114,18 @@
                 My specializations so far have been full-stack web development and data engineering with a bit of a machine learning emphasis. I'm interested in learning more about human-computer interaction,
                 data engineering, and machine learning.
             </p>
+            </div>
         {/if}
-        <button class="hover:opacity-75 m-1" on:click={toggleTechnical}>technical skills  v</button>
+        <button class="hover:opacity-75 m-1" on:click={toggleTechnical}>{technicalButton}</button>
         {#if technicalVis}
+            <div transition:slide>
             <p>
                 <strong>Fluent:</strong> C++, SQL, Python<br>
                 <strong>Very Conversational:</strong> Java, ARM Assembly, Linux(Ubuntu/Debian), Qt Creator, SvelteKit, TailwindCSS<br>
                 <strong>Conversational:</strong> Git, HTML, CSS, JavaScript, PHP, R, Apache2, nginx, Redis, Quart/Flask<br>
                 <strong>Learning:</strong> Rust, Apache Spark, PowerShell, Kafka
             </p>
+            </div>
         {/if}
     </div >
 </div>
